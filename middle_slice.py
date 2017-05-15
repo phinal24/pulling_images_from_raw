d57cab4c057c1174f6b3d523f460e8d3e7afce5f
phinal24 <phinal24@users.noreply.github.com>
2017-05-15 14:17:24 -0500

    Update middle_slice.py

--- middle_slice.py
+++ middle_slice.py
@@ -3,6 +3,7 @@ import os, sys
 import glob
 import shutil
 import MySQLdb
+import numpy as np
 
 ### Target location of files on storage server
 directory="/mnt/glusterfs"
@@ -36,12 +37,18 @@ try:
 			zdim = int(row[10])
 			try:
 				year=date.rsplit('-',2)[0]
-        recon_number=remote_folder.rsplit('_',1)[-1]
+        			recon_number=remote_folder.rsplit('_',1)[-1]
 				new_directory="%s/%s" % (directory,year)
 				new_sample_folder="%s/%s" % (new_directory,sample)
-        new_recon_raw="%s/RECON_%s/%s_%s_%s_%sx%sx%s.raw" % (new_sample_folder,recon_number,sample,voxel,bitrate,xdim,ydim,zdim)
+        			new_recon_raw="%s/RECON_%s/%s_%s_%s_%sx%sx%s.raw" % (new_sample_folder,recon_number,sample,voxel,bitrate,xdim,ydim,zdim)
 				if os.path.exists(new_recon_raw):
 					### ENTER IN THE CODE FOR EXTRACTING THE IMAGE ###
+					volume=np.loadfile(new_recon_faw)		#check syntax
+					array=np.reshape(volume,(zdim,ydim,xdim))	#check syntax
+					z_center=int(zdim) // 2				#check syntax
+					slice50=array[(zdim - 1):zdim]			#check syntax
+					slice50=np.reshape(slice50,(ydim,xdim))		#check syntax
+					saveimage(slice50,uint8,png_slice50)		#check syntax twice
 					print("Successfully extracted slice@50 from %s/RECON_%s" % (sample,recon_number))
 			except:
 				print("Problem with %s" % (sample))
